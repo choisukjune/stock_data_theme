b1e6831adcba0ddb6e8486a118b726084946ec9f
tjrwns <58602971+tjrwns@users.noreply.github.com>
2021-01-07 08:38:22 +0900

    .

--- Server_start__electron.bat
+++ Server_start__electron.bat
@@ -1,2 +1,2 @@
 @echo off
-call D:\workspace_csj\Binary\electron\electron-v10.2.0-win32-x64\electron.exe --js-flags="--expose-gc" .\
+call ..\Binary\electron\electron-v10.2.0-win32-x64\electron.exe --js-flags="--expose-gc" .\
